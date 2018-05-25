6157fa6bc355dbb25b90df0658348951e9e69b91
Sunil Hirole <sunilhirole@gmail.com>
2018-05-25 14:57:44 +0530

    Feature: Added socket.io for chat functionality

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+server.listen(8000, function(){
+  console.log('listening on *:8000');
+});
